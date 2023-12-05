a92a072eeea8d289b1c4832bb0dacafc06dd14f2
Eliúde Vemba <sweeteliude@gmail.com>
2023-12-05 21:21:42 +0100

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Eliude Vemba on 05/12/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
